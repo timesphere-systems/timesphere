9ce1645e377a575d64b1d8cb151bfe4c075e209f
the-Jamz <james@jamz.dev>
2024-03-06 18:00:55 +0000

    adjust structure

--- back/README.md
+++ back/README.md
@@ -7,7 +7,7 @@ To create a local environment and run the backend (within the `back` directory):
 - `python3 -m venv venv`
 - `source venv/bin/activate`
 - `pip install -r requirements.txt`
-- `uvicorn app.api.server:app --reload`
+- `uvicorn api.main:app --reload`
 
 After the initial setup you'll just need to activate the virtual environment and run the server in future:
 `source venv/bin/activate`
--- back/api/main.py
+++ back/api/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-from .routers import consultant,holidayrequest, timesheet
+from .routers import consultant, holiday, timesheet
 
 app = FastAPI()
 
-
 app.include_router(consultant.router)
-app.include_router(holidayrequest.router)
+app.include_router(holiday.router)
 app.include_router(timesheet.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
