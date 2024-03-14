51eb9a825473e8a7e94b3f5ae5f7f31d18464e2a
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-14 06:21:58 +0000

    Fix SQL Code Format and Added role Column to users table

--- back/api/database/schema/insert_table_data_script.sql
+++ back/api/database/schema/insert_table_data_script.sql
@@ -2,17 +2,17 @@
 
 --setting up Status, TimeEntryType and Role Values
 --this is stored inorder so the id is consistant with the ENUMs defined
-INSERT INTO Status (STATUS_TYPE)
+INSERT INTO status (status_type)
 VALUES 
   ('WAITING'),
   ('APPROVED'),
   ('DENIED');
-INSERT INTO TimeEntryType (ENTRY_TYPE)
+INSERT INTO time_entry_type (entry_type)
 VALUES
   ('WORK'),
   ('SICK'),
   ('HOLIDAY');
-INSERT INTO Role (ROLE_TYPE)
+INSERT INTO Role (role_type)
 VALUES
   ('CONSULTANT'),
   ('MANAGER'),
