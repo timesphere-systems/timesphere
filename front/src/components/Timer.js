ed1748f070ac53dcc253a26556cc6f53430c87e3
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-16 21:31:51 +0000

    Changed rem to px && used camelCase/arrow function expressions with let

--- front/src/components/Timer.js
+++ front/src/components/Timer.js
@@ -4,13 +4,13 @@ import TimerIcon from '../assets/icons/TimerIcon.svg';
 
 const TIMER = styled.div `
     display: flex;
-    width: 13rem;
-    height: 4rem;  
-    padding: 1rem;
+    width: 200px;
+    height: 70px;  
+    padding: 15px;
     justify-content: center;
     align-items: center;
-    gap: 1.25rem;
-    border-radius: 1rem;
+    gap: 15px;
+    border-radius: 15px;
     background: #9DA1AD;
 `
 
@@ -18,30 +18,27 @@ const TIMEREADING = styled.p `
     color: #FFF;
     text-align: center;
     font-family: Inter;
-    font-size: 2rem;
+    font-size: 32px;
     font-style: normal;
     font-weight: 700;
     line-height: normal;
 `
 
-function zeroPad(num) {
-    return String(num).padStart(2, "0");
-}
 
-function twodigit_format(num){
+let formatTwoDigits = (num) => {
     if (num > 9){
         return String(num);
     }
-    return(zeroPad(num));
+    return String(num).padStart(2, "0");
 }
 
 const getTimePassed = (start_time, setHours, setMinutes, setSeconds) =>{
     let time = Date.now() - Date.parse(start_time);
 
     //bellow converts the time from milliseconds to hours and minutes
-    setHours(twodigit_format(Math.floor(time / (1000 * 60 * 60)) % 24));
-    setMinutes(twodigit_format(Math.floor((time / 1000 / 60) % 60)));
-    setSeconds(twodigit_format(Math.floor((time / 1000) % 60)));
+    setHours(formatTwoDigits(Math.floor(time / (1000 * 60 * 60)) % 24));
+    setMinutes(formatTwoDigits(Math.floor((time / 1000 / 60) % 60)));
+    setSeconds(formatTwoDigits(Math.floor((time / 1000) % 60)));
 }
 
 const Timer = ({start_time}) => {
