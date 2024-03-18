85fbb2514c4b01135b97487c57c1c9911202b56c
the-Jamz <james@jamz.dev>
2024-03-18 12:38:26 +0000

    database dependency

--- back/api/dependencies.py
+++ back/api/dependencies.py
@@ -1,8 +1,21 @@
 """This module contains the dependencies for the API."""
 from functools import lru_cache
+import psycopg_pool
 from .config import Settings
 
 @lru_cache
 def get_settings() -> Settings:
     """Get the settings for the API."""
     return Settings()
+
+@lru_cache
+def get_connection_pool() -> psycopg_pool.ConnectionPool:
+    """Get a connection pool for the database."""
+    settings = get_settings()
+    print("making new pool")
+    return psycopg_pool.ConnectionPool(
+        f"dbname={settings.postgres_db_name} \
+            user={settings.postgres_db_user} \
+            password={settings.postgres_db_password} \
+            host={settings.postgres_db_host}"
+    )
