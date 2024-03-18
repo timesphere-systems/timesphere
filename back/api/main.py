6e7e53d409ba0b7163625d750caf9a8b74761413
the-Jamz <james@jamz.dev>
2024-03-18 23:59:52 +0000

    database fixes, create consultant and create user implementation

--- back/api/main.py
+++ back/api/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from .consultant.routes import router as consultant
 from .holiday.routes import router as holiday
 from .timesheet.routes import router as timesheet
+from .user.routes import router as user
 
 app = FastAPI()
 
 app.include_router(consultant)
 app.include_router(holiday)
 app.include_router(timesheet)
+app.include_router(user)
--- back/api/models.py
+++ back/api/models.py
@@ -1,9 +1,3 @@
 """JSON models for use throughout the application."""
 # pylint: disable=too-few-public-methods
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    """Model for a user."""
-    name: str
-    email: str
+# from pydantic import BaseModel
