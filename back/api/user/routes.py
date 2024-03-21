8ab820dce791d929b77bfe175bda2c9d679536a2
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-21 11:13:58 +0000

    Changed Create Methods for type casting

--- back/api/user/routes.py
+++ back/api/user/routes.py
@@ -1,5 +1,5 @@
 """Routes for the user API."""
-from typing import Annotated
+from typing import Annotated, cast
 from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from psycopg_pool import ConnectionPool
@@ -21,12 +21,15 @@ def create_user(request: models.User,
     Args:
         request (models.User): The user's details."""
     with pool.connection() as connection:
-        user_id = None
+        user_id: int = 0
         try:
-            user_id = connection.execute("""
+            row = connection.execute("""
                 INSERT INTO users (firstname, lastname, email, user_role)
                 VALUES (%s, %s, %s, %s) RETURNING id""",
                 (request.firstname, request.lastname, request.email, request.user_role)).fetchone()
+            if row is None:
+                raise ValueError("Failed to create timesheet")
+            user_id = cast(int, row[0])
         except ForeignKeyViolation:
             return JSONResponse(
                 status_code=status.HTTP_400_BAD_REQUEST,
