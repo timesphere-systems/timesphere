30e17d925c347ed9c888c76d382dea0e9ac5643b
ec21413 <ec2413@qmul.ac.uk>
2024-03-21 15:31:03 +0000

    update

--- front/src/components/DashboardTable.js
+++ front/src/components/DashboardTable.js
@@ -2,19 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 import { useState } from 'react'
 
+
 const TIMESHEET = styled.table`
-    border-radius: 9px;
     margin: auto;
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 100%;
 `
+
 const HEADERS = styled.thead`
     color: white;
     font-weight: bold;
     background-color: rgba(54, 54, 54, 0.95);
+    border-top-right-radius: 9px;
+    border-top-left-radius: 9px;
+    border-collapse: collapse; /* Collapsing borders */
+    overflow: hidden; /* Hiding overflow content */
 `
 
 const TR = styled.tr`
@@ -26,6 +32,7 @@ const TH = styled.th`
     width: 100px;
     border: 1px solid rgba(91, 91, 91, 1); 
 `
+
 const TD = styled.td`
     padding: 10px;
     width: 100px;
@@ -35,11 +42,15 @@ const TD = styled.td`
     text-align: center;
 `
 
+const Dropdown = styled.select`
+    width: 100%;
+`
 
 
 
 const DashboardTable = () => {
 
+    // Get dates for the current week (Mon - Fri)
     const getWeekDates = () => {
         const today = new Date();
         const monday = new Date(today);
@@ -47,16 +58,26 @@ const DashboardTable = () => {
         const weekDates = [...Array(5)].map((_, index) => {
             const date = new Date(monday);
             date.setDate(date.getDate() + index);
-            return date;
+            return { date, status: 'Working' };
         })
         return weekDates;
     };
 
-    const [weekDates] = useState(getWeekDates());
+    const [weekDates, setWeekDates] = useState(getWeekDates());
+    
+
+    const handleStatusChange = (index, value) => {
+        const newWeekDates =  [...weekDates];
+        newWeekDates[index] = {
+            ...newWeekDates[index],
+            status: value
+        };
+        setWeekDates(newWeekDates);
+    };
 
     return (
-        <TIMESHEET>
-           <HEADERS>
+    <TIMESHEET>
+        <HEADERS>
                 <TR>
                     <TH></TH>
                     <TH>Date</TH>
@@ -65,20 +86,26 @@ const DashboardTable = () => {
                     <TH>Clock-Out</TH>
                     <TH>Hours</TH>
                 </TR>
-           </HEADERS>
-           <tbody>
-                {weekDates.map((date, index) => (
+        </HEADERS>
+        <tbody>
+                {weekDates.map((row, index) => (
                     <TR key={index}>
                         <TD>{['Monday','Tuesday','Wednesday','Thursday','Friday'][index]}</TD>
-                        <TD>{date.toLocaleDateString()}</TD>
-                        <TD></TD>
+                        <TD>{row.date.toLocaleDateString()}</TD>
+                        <TD>
+                            <Dropdown value={row.status} onChange={(e) => handleStatusChange(index, e.target.value)}>
+                                <option value="Working">Working</option>
+                                <option value="Sick">Sick</option>
+                                <option value="Holiday">Holiday</option>
+                            </Dropdown>
+                        </TD>
                         <TD></TD>
                         <TD></TD>
                         <TD></TD>
                     </TR>
                 ))}
-           </tbody>
-        </TIMESHEET>
+        </tbody>
+    </TIMESHEET> 
     )
 }
 
