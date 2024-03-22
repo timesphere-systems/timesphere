7f12ba669d2ca3b4929c602b41ec098f1aba6d7d
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-22 20:22:26 +0000

    Fixing pylint issues

--- back/api/timesheet/routes.py
+++ back/api/timesheet/routes.py
@@ -48,7 +48,8 @@ def submit_timesheet(timesheet_id: int,
                 SET approval_status = (SELECT id FROM approval_status WHERE status_type='SUBMITTED')
                 WHERE id = (%s);""",
                 (timesheet_id,))
-            #if statement check the number of modified rows to ensure a valid timesheet ID was provided
+            #if statement check the number of modified rows
+            #to ensure a valid timesheet ID was provided
             # number of modified rows == cursor.rowcount
             if cursor.rowcount == 1:
                 return JSONResponse(
