1aee6290b631a81734c1573b56e6a8bbe8f0e11c
ec21413 <ec2413@qmul.ac.uk>
2024-03-23 17:04:11 +0000

    update

--- front/src/components/ProfileSidebar.js
+++ front/src/components/ProfileSidebar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import ExitIcon from '../assets/icons/ExitIcon.svg';
+import EmailIcon from '../assets/icons/EmailIcon.svg';
+import TimezoneIcon from '../assets/icons/TimezoneIcon.svg';
+
 
 const SIDEBAR = styled.nav`
     position: absolute;
@@ -7,16 +11,22 @@ const SIDEBAR = styled.nav`
     right: 0;
     display: flex;
     flex-direction: column;
+    //align-items: end;
     border-left: 1px solid black;
     border-top-left-radius: 9px;
     border-bottom-left-radius: 9px;
     padding-inline: 20px;
-    padding-top: 50px;
+    padding-top: 10px;
     background-color: white; 
-    height: 91.5%;
+    height: 98%;
     width: 200px; 
     
 `
+
+const ExitButton = styled.img`
+    cursor: pointer;
+`
+
 
 const PFP = styled.img`
     width: 100px;
@@ -25,13 +35,22 @@ const PFP = styled.img`
     object-fit: cover; 
     margin: 0 auto; 
     display: block; 
-    border: 2px solid black;
+    border: 2px solid #1B143E;
 `
 
 const NAME = styled.h1`
-    color: blue;
+    color: black;
     font-size: 22px;
+    text-align: center;
 `
+
+const INFO_HEADER = styled.div`
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
 
 const EMAIL = styled.p`
 `
@@ -41,12 +60,13 @@ const TIMEZONE = styled.p`
 
 
 
-const ProfileSidebar = ({profileImg, name, email, timezone}) => {
+const ProfileSidebar = ({profileImg, name = "Matthew", email, timezone}) => {
 
     return (
       <div>
         <SIDEBAR>
             <div>
+                <ExitButton src={ExitIcon} alt="exit" />
             </div>
             <div>
                 <PFP src={profileImg} alt="profile Pic" />
@@ -55,11 +75,11 @@ const ProfileSidebar = ({profileImg, name, email, timezone}) => {
                 <NAME>{name}</NAME>
             </div>
             <div>
-                <p style={{fontWeight:'bold'}}>Email Address</p>
+                <INFO_HEADER><img src={EmailIcon}/>Email Address</INFO_HEADER>
                 <EMAIL>{email}</EMAIL>
             </div>
             <div>
-                <p style={{fontWeight:'bold'}}>Timezone</p>
+                <INFO_HEADER><img src={TimezoneIcon}/>Timezone</INFO_HEADER>
                 <TIMEZONE>{timezone}</TIMEZONE>
             </div>
         </SIDEBAR>
