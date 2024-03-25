a0fde205b6033f19c1c8a3e87d5178c4925d48be
Rayan Bahadur <rayanbahadur16@gmail.com>
2024-03-25 16:40:14 +0000

    Refactor ModalWrapper and WeeklyHoursTable components for Reusability

--- front/src/components/ModalWrapper.js
+++ front/src/components/ModalWrapper.js
@@ -15,58 +15,15 @@ const Overlay = styled.div`
     visibility: ${({ isVisible }) => isVisible ? 'visible' : 'hidden'};
 `;
 
-const TIMESHEET = styled.table`
-    margin: auto;
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-collapse: collapse;
-    overflow: hidden;
-    border-radius: inherit;
-`
-
-const HEADERS = styled.thead`
-    color: white;
-    font-weight: bold;
-    background-color: rgba(54, 54, 54, 0.95);
-    border-top-right-radius: 9px;
-    border-top-left-radius: 9px;
-    border-collapse: collapse; 
-    overflow: hidden;
-`
-
-const TR = styled.tr`
-    display: flex;
-`
-
-const TH = styled.th`
-    padding: 10px;
-    width: 120px;
-    border: 1px solid rgba(91, 91, 91, 1); 
-    justify-self: center;
-`
-
-const TD = styled.td`
-    padding: 10px;
-    width: 120px;
-    color: white;
-    border: 1px solid rgba(91, 91, 91, 1);
-    background-color: rgba(54, 54, 54, 1);
-    font-weight: 300;
-    text-align: center;
-`
-
-
 const OVERLAY_HEADER = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-bottom: 20px;
 
     h2{
         margin-top: 0;
+        margin-bottom: 0;
     }
 `;
 
@@ -74,13 +31,6 @@ const CLOSEBUTTON = styled.button`
     background: none;
     border: none;
     cursor: pointer;
-`;
-
-const OVERLAY_CONTAINER = styled.div`
-    position: relative;
-    margin: auto;
-    border-radius: 9px;
-    overflow: hidden;
 `;
 
 const OVERLAY_CONTENT = styled.div`
@@ -94,41 +44,17 @@ const OVERLAY_CONTENT = styled.div`
 `;
 
 
-const ModalWrapper = ({ isVisible, toggleOverlay }) => { 
+const ModalWrapper = ({ isVisible, toggleOverlay, title, children}) => { 
   return (
     <Overlay isVisible={isVisible}>
-                <OVERLAY_CONTENT>
-                    <OVERLAY_HEADER>
-                        <h2>Timesheet</h2>
-                        <CLOSEBUTTON onClick={toggleOverlay}><IoClose style={{height: '25px', width: '25px'}}/></CLOSEBUTTON>
-                    </OVERLAY_HEADER>
-                    {/* Add the timesheet form here */}
-                        <OVERLAY_CONTAINER>
-                        <TIMESHEET>
-                            <HEADERS>
-                                    <TR>
-                                        <TH></TH>
-                                        <TH>Date</TH>
-                                        <TH>Status</TH>
-                                        <TH>Clock-In</TH>
-                                        <TH>Clock-Out</TH>
-                                        <TH>Hours</TH>
-                                    </TR>
-                            </HEADERS>
-                            <tbody>
-                                <TR>
-                                    <TD></TD>
-                                    <TD></TD>
-                                    <TD></TD>
-                                    <TD></TD>
-                                    <TD></TD>
-                                    <TD></TD>
-                                </TR>
-                            </tbody>
-                        </TIMESHEET> 
-                    </OVERLAY_CONTAINER>
-                </OVERLAY_CONTENT>
-            </Overlay>
+        <OVERLAY_CONTENT>
+            <OVERLAY_HEADER>
+                <h2>{title}</h2>
+                <CLOSEBUTTON onClick={toggleOverlay}><IoClose style={{height: '25px', width: '25px'}}/></CLOSEBUTTON>
+            </OVERLAY_HEADER>
+            {children}
+        </OVERLAY_CONTENT>
+    </Overlay>
   );
 }
 
