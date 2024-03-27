9a6384a1d5dd1df791dd7f8b01372d37a2249313
ec21413 <ec2413@qmul.ac.uk>
2024-03-27 14:10:32 +0000

    added basic css styling

--- front/src/components/EditToggleButton.js
+++ front/src/components/EditToggleButton.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// hide default html checkbox
 const INPUT = styled.input`
-
+    opacity: 0;
+    width: 0;
+    height: 0;
 `
 
+// box around slider
 const SWITCH = styled.label`
     font-size: 17px;
     position: relative;
     display: inline-block;
     width: 3.5em;
     height: 2em;
-    border: 1px solid black;
 `
 
+
 const SLIDER = styled.span`
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    transition: .4s;
+    border-radius: 30px;
 `
 
 const EditToggleButton = () => {
