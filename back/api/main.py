74bad9947f0199ef0a715c68e4d4616869aec9a7
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-27 07:54:41 +0000

    Initalise and set up manager router

--- back/api/main.py
+++ back/api/main.py
@@ -4,6 +4,7 @@ from .consultant.routes import router as consultant
 from .holiday.routes import router as holiday
 from .timesheet.routes import router as timesheet
 from .user.routes import router as user
+from .manager.routes import router as manager
 
 
 app = FastAPI()
@@ -12,3 +13,4 @@ app.include_router(consultant)
 app.include_router(holiday)
 app.include_router(timesheet)
 app.include_router(user)
+app.include_router(manager)
