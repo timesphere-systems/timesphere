4b86364cf60d06ad0b7308f934606dae110ffeec
ec22962 <e.vert@se22.qmul.ac.uk>
2024-03-27 19:56:05 +0000

    Added navbar, notifications and page selector components

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,90 +1,82 @@
 import React from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/icons/Logo.svg'
+import Notifications from './Notifications'
+import PageSelector from './PageSelector'
 
 const NAV = styled.nav`
     display: flex;
-    width: 95%;
+    flex-flow: row nowrap;
+    width: 100%;
     height: 65px;
-    padding: 0px 12px 0px 26px;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const LOGO = styled.div`
+    display: flex;
+    flex: 1;
     justify-content: flex-start;
     align-items: center;
-    gap: 58px;
-    flex-shrink: 0;
 
-    border-radius: 9999px;
-    border-bottom: 1px solid var(--light-moon-mist, #E0E2D9);
-    background: var(--light-nero, #FFF);
+    margin-left: 2rem;
+
+    cursor: pointer;
+    user-select: none;
 `
 
-const HOME = styled.p`
-    font-weight: 600;
-    width: 82px;
-    height: 36px;
-
+const SELECTOR_CONTAINER = styled.div`
     display: flex;
+    flex: 1;
     align-items: center;
     justify-content: center;
-
-    border-radius: 32px;
-    border: 1px solid #EFEFEF;
-
-    background: radial-gradient(412.6% 100% at 0% 50%, #FD585D 0%, #D058D4 100%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-`
-
-const LINKS = styled.p`
-    color: #000000;
-    cursor: pointer;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    gap: 8px;
 `
 
 const R_CONTAINER = styled.div`
     display: flex;
+    flex: 1;
     align-items: center;
-    justify-content: space-evenly;
-    margin-left: auto;
-    gap: 12px;
+    justify-content: flex-end;
+    gap: 32px;
+
+    margin-right: 2rem;
+    max-width: min-content;
 `
 
-const PFP = styled.div`
+const PFP = styled.img`
     cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
 
-    width: 40px;
-    height: 40px;
-    border-radius: 1000px;
+    width: 43px;
+    height: 43px;
+    border-radius: 100%;
+    object-fit: cover;
 
-    background: var(--light-Nevada, #60696C);
-
-    font-size: 18px;
-    font-weight: 600;
-    color: #FFF;
+    border: 2px solid #1B143E;
 `
+
 
 const Navbar = () => {
   return (
-      <NAV>
-        
+    <NAV>
         <Link to={"/"} style={{textDecoration: 'none', color: 'inherit'}}>
-            <LINKS>
-                <HOME>Home</HOME>
-            </LINKS>
+            <LOGO>
+                <img src={Logo} alt="logo" />
+            </LOGO>
         </Link>
 
+        <SELECTOR_CONTAINER>
+            <PageSelector />
+        </SELECTOR_CONTAINER>
+
         <R_CONTAINER>
+            <Notifications hasNew={true}/>
+
             <Link to={"/profile"} style={{textDecoration: 'none', color: 'inherit'}}>
-                <PFP>
-                    <p>E</p>
-                </PFP>
+                <PFP src={"https://i.imgflip.com/122vae.jpg?a475128"} />
             </Link>
         </R_CONTAINER>
     </NAV>
