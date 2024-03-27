f380dab8d51fbde15975b697865a589fa848cea1
ec21413 <ec2413@qmul.ac.uk>
2024-03-27 16:44:01 +0000

    added error icon for the uneditable mode on the toggle. imported and applied to component

--- front/src/components/EditToggleButton.js
+++ front/src/components/EditToggleButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import EditIcon from '../assets/icons/EditIcon.svg';
+import ErrorIcon from '../assets/icons/ErrorIcon.svg';
 
 // hide default html checkbox
 const INPUT = styled.input`
@@ -13,8 +14,8 @@ const INPUT = styled.input`
 const SWITCH = styled.label`
     position: relative;
     display: inline-block;
-    width: 54px;
-    height: 30px;
+    width: 80px;
+    height: 40px;
 `
 
 // slider styling and toggle animation
@@ -25,45 +26,50 @@ const SLIDER = styled.span`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: ${props => props.checked ? '#ccc' : '#ccc'};
-    transition: background-color .4s, box-shadow .4s;
+    background-color: #ccc;
     border-radius: 30px;
+    //background-color: ${props => props.checked ? '#ccc' : '#ccc'};
+    //transition: background-color .4s, box-shadow .4s;
+    
 
     &::before {
         position: absolute;
         content: "";
-        height: 30px;
-        width: 30px;
+        height: 40px;
+        width: 40px;
         border-radius: 20px;
         left: 0;
         bottom: 0;
         background-color: #1B143E;
-        transition: transform .2s;
-        transform: ${props => props.checked ? 'translateX(1.5em)' : 'translateX(0)'};
+        transition: transform .3s;
+        transform: ${props => props.checked ? 'translateX(40px)' : 'translateX(0)'};
     }
 
-    &::after {
-        //content: url(${EditIcon}); // Use the imported edit.svg file
-        position: absolute;
-        //top: 50%;
-        //left: 50%;
-        //transform: translate(-50%, -50%);
-        opacity: ${props => props.checked ? '1' : '0'}; // Show/hide the image based on checked state
-        transition: opacity .4s;
-    }
 `
-
 const EDIT_ICON = styled.img`
     cursor: pointer;
     position: absolute;
     top: 50%;
-    left: 73%;
+    left: 75%;
     transform: translate(-50%, -50%);
     height: 20px; 
     width: 20px; 
-    visibility: ${props => props.checked ? 'visible' : 'hidden'};
+    visibility: ${props => props.checked ? 'visible' : 'visible'};
 
 `
+const ERROR_ICON = styled.img`
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    left: 25%;
+    transform: translate(-50%, -50%);
+    height: 20px; 
+    width: 20px; 
+    visibility: ${props => props.checked ? 'visible' : 'visible'};
+
+`
+
+
 
 const EditToggleButton = () => {
     const [checked, setChecked] = React.useState(false);
@@ -78,6 +84,7 @@ const EditToggleButton = () => {
                 <INPUT type="checkbox" checked={checked} onChange={handleToggle}/>
                 <SLIDER checked={checked}></SLIDER>
                 <EDIT_ICON src={EditIcon} alt="Edit" checked={checked}/>
+                <ERROR_ICON src={ErrorIcon} alt="" checked={checked}/>
             </SWITCH>
             
         </div>
