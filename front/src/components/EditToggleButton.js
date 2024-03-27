9a56d0a8ef3cf7c85e57db7bea3d8fab813eecff
ec21413 <ec2413@qmul.ac.uk>
2024-03-27 14:00:50 +0000

    render html elements to create a switch label with an input type checkbox - basis of a toggle button

--- front/src/components/EditToggleButton.js
+++ front/src/components/EditToggleButton.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const INPUT = styled.input`
+
+`
+
+const SWITCH = styled.label`
+    font-size: 17px;
+    position: relative;
+    display: inline-block;
+    width: 3.5em;
+    height: 2em;
+    border: 1px solid black;
+`
+
+const SLIDER = styled.span`
+`
 
 const EditToggleButton = () => {
+
   return (
-    <div>EditToggleButton</div>
-  )
-}
-export default EditToggleButton
+    <div>
+        <SWITCH>
+            <INPUT type="checkbox"/>
+            <SLIDER></SLIDER>
+        </SWITCH>
+    </div>
+  );
+};
+
+export default EditToggleButton;
