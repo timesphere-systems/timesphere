b87884aa6c49a11001a19da6b1008ab8685cee30
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-03-28 17:34:52 +0000

    Implemented Get Timesheet method

--- back/api/timesheet/models.py
+++ back/api/timesheet/models.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from enum import Enum
 from pydantic import BaseModel
+from ..models import ApprovalStatus
 
 
 class TimesheetStatus(Enum):
@@ -14,18 +15,23 @@ class TimesheetStatus(Enum):
 
 class TimeEntryType(Enum):
     """Enum for time entry types."""
-    WORK = 1
-    SICK = 2
-    HOLIDAY = 3
+    WORK = 'WORK'
+    SICK = 'SICK'
+    HOLIDAY = 'HOLIDAY'
 
 class TimeEntry(BaseModel):
     """Model for the time entries."""
+    id: int
     entry_type: TimeEntryType
-    start: datetime
-    end: datetime
+    start_time: datetime
+    end_time: datetime | None
+    timesheet_id: int
 
 class Timesheet(BaseModel):
     """Model for timesheets."""
+    created: datetime
+    submitted: datetime | None
     start: datetime
-    entries: list[TimeEntry]
-    status: TimesheetStatus
+    entries: list[int] | list[None]
+    consultant_id: int
+    approval_status: ApprovalStatus
