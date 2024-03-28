77591c474e45739d8f3bf9179f2d21169d929386
Rayan Bahadur <rayanbahadur16@gmail.com>
2024-03-28 11:34:22 +0000

    Updated SubmitButton

--- front/src/components/SubmitButton.js
+++ front/src/components/SubmitButton.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+
+import SubmitIcon from '../assets/icons/Submit.svg';
+import CheckIcon from '../assets/icons/Checkmark.svg';
 
 const SUBMITBUTTON = styled.button`
     border: black;
@@ -15,18 +18,24 @@ const SUBMITBUTTON = styled.button`
 
     width: ${props => props.width !== undefined ? props.width : '300px'};
     height: ${props => props.height !== undefined ? props.height : '100px'};
-    background-color: ${props => (props.clickable ? '#1B143E' : '#9DA1AD')};
+    background-color: ${(props) => {
+        if (!props.clickable) return '#9DA1AD';
+        return props.isSubmitted ? '#6BD583' : '#1B143E';
+    }};
     cursor: ${props => props.clickable ? 'pointer' : 'not-allowed'};
 `
-const SubmitButton = ({onClick, width, height, clickable, icon}) => {
+const SubmitButton = ({onClick, width, height, clickable}) => {
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
     
     // If clickable is false, then we prevent anything from happening when the button is clicked
     let handleClick = (e) => {
-        if (!clickable) {
+        if (!clickable || isSubmitted) {
             e.preventDefault();
             return;
         } else {
             onClick();
+            setIsSubmitted(true);
         }
     }
     return (
@@ -34,10 +43,20 @@ const SubmitButton = ({onClick, width, height, clickable, icon}) => {
             width={width}
             height={height}
             clickable={clickable}
+            isSubmitted={isSubmitted}
             onClick={handleClick}
         >
-            <p>Submit</p>
-            <img src={icon} alt="submit button icon" />
+            {isSubmitted ? (
+                <>
+                    <p>Submitted</p>
+                    <img src={CheckIcon} alt="Check icon" />
+                </>
+            ) : (
+                <>
+                    <p>Submit</p>
+                    <img src={SubmitIcon} alt="Submit icon" />
+                </>
+            )}
         </SUBMITBUTTON>
       )
 }
