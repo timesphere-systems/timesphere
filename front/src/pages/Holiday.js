f960550270638f4e61442a15b911b4aa4c7cae06
Khadijah <khadijahdarragi2003@gmail.com>
2024-04-06 17:26:31 +0100

    added useEffect hook to get token locally

--- front/src/pages/Holiday.js
+++ front/src/pages/Holiday.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import styled from 'styled-components'
 import Selector from '../components/Selector'
@@ -54,6 +54,7 @@ const Holiday = () => {
     let handleStatusChange = (selectedStatus) => {
         setApprovalStatus(selectedStatus);
     };
+
 
     React.useEffect(() => {
         let getToken = async () => {
