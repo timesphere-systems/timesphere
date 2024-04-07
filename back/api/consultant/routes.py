0d7bd27b7496e833a6be0a5129c4ac5cf22d37fa
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-04-07 18:31:39 +0100

    Moved search method to be in consultant/routes.py and changled sql query to return consultant.id

--- back/api/consultant/routes.py
+++ back/api/consultant/routes.py
@@ -21,6 +21,43 @@ router = APIRouter(
     tags=["consultant"],
 )
 
+@router.get("/search", status_code=status.HTTP_200_OK)
+def search_consultant(search_query: str,
+                      pool: Annotated[ConnectionPool, Depends(get_connection_pool)],
+                      current_user: Annotated[User, Security(get_current_user)]
+                      ) -> JSONResponse:
+    """Search for a Consultant
+
+    Requires for the current user to have the finance user role or manager role
+
+    Args:
+        query (str): The name or email to search for the consultant
+        pool (Annotated[ConnectionPool, Depends(get_connection_pool)]): The connection pool.
+    Returns:
+        JSONResponse
+    """
+    if current_user.details.user_role != 3 and current_user.details.user_role != 2:
+        return JSONResponse(
+            status_code=status.HTTP_403_FORBIDDEN,
+            content={"message": "You do not have permission to search for a consultant"}
+        )
+    search_query = '%' + search_query + '%'
+    consultants = []
+    with pool.connection() as connection:
+        with connection.cursor() as cursor:
+            row = cursor.execute(
+                """SELECT consultants.id
+                   FROM users, consultants
+                   WHERE consultants.user_id = users.id
+                   AND (CONCAT(users.firstname, ' ', users.lastname) LIKE %(search_query)s
+                   OR users.email LIKE %(search_query)s)"""
+                   , {'search_query': search_query}).fetchall()
+            consultants = [consultant[0] for consultant in row]
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"consultants": consultants}
+    )
+
 @router.post("", status_code=status.HTTP_201_CREATED)
 def create_consultant(request: models.CreateConsultant,
                       pool: Annotated[ConnectionPool, Depends(get_connection_pool)],
