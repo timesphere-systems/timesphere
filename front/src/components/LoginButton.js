33395e8ff5a41370e35c48c035f689045355f528
James M <james@jamz.dev>
2024-04-09 18:23:33 +0100

    Revert "Token is now fetched in loginbutton and put in localstorage"
    This reverts commit f95ce14e2c226af1b8fc3dcd1fc3126de844cee1.

--- front/src/components/LoginButton.js
+++ front/src/components/LoginButton.js
@@ -27,25 +27,7 @@ const LOGINBUTTON = styled.button`
 `
 
 const LoginButton = ({width, height}) => {
-    const { isAuthenticated, loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
-
-    React.useEffect(() => {
-        let getToken = async () => {
-            if (isAuthenticated) {
-                let token = await getAccessTokenSilently(
-                    {authorizationParams: {        
-                        audience: "https://timesphere.systems/api",
-                        redirect_uri: "http://localhost:3000",
-                        scope: "timesphere:admin"
-                    }});
-                console.log(token);
-                
-                if (localStorage.getItem("token") === null) localStorage.setItem("token", token)
-                else console.log("Token already exists")
-            }
-        }
-        getToken();
-      }, [getAccessTokenSilently, isAuthenticated])
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     let handleClick = () => {
         isAuthenticated ? logout({ 
